82f3fe465d86f4e02b8679d97266acf3222631c8
Darik ONeil <d.oneil@columbia.edu>
2023-02-12 16:00:10 -0500

    work on event / trace processing

--- src/CalSciPy/event_processing.py
+++ src/CalSciPy/event_processing.py
@@ -1,8 +1,46 @@
-def lol():
-    """
-    Testing func
+from __future__ import annotations
+import numpy as np
 
-    :return: haha
-    """
-    A = 1
+
+def bin_data():
     return
+# TODO PASTE, DOCUMENT, UNIT TEST
+
+
+def gaussian_smooth_firing_rates(firing_matrix: np.ndarray) -> np.ndarray:
+    return
+# TODO PASTE, DOCUMENT, UNIT TEST
+
+
+def calculate_firing_rate(spike_probability_matrix: np.ndarray, frame_rate: float = 30) -> np.ndarray:
+    """
+
+    :param spike_probability_matrix: matrix of n neuron x m samples where each element is the probability of a spike
+    :type spike_probability_matrix: np.ndarray
+    :param frame_rate: frame rate of dataset
+    :type frame_rate: float = 30
+    :return: matrix of firing rates
+    :rtype: np.ndarray
+    """
+    return spike_probability_matrix * frame_rate
+# TODO UNIT TEST
+
+
+def calculate_mean_firing_rate(firing_matrix: np.ndarray) -> np.ndarray:
+    """
+    Calculate mean firing rate
+
+    :param firing_matrix: matrix of n neuron x m samples where each element is either a spike or an
+    instantaneous firing rate
+    :type firing_matrix: np.ndarray
+    :return: 1-D vector of mean firing rates
+    :rtype: np.ndarray
+    """
+    return np.nanmean(NeuralData, axis=1)
+# TODO UNIT TEST
+
+
+def normalize_firing_rates(firing_matrix: np.ndarray) -> np.ndarray:
+    return
+# TODO PASTE, DOCUMENT, UNIT TEST
+
--- src/CalSciPy/trace_processing.py
+++ src/CalSciPy/trace_processing.py
@@ -1,36 +1,12 @@
 from __future__ import annotations
-import sys
 import numpy as np
-import itertools
 from numba import jit
+from obspy.signal.detrend import polynomial
 
 
-def calculate_firing_rate(spike_probability_matrix: np.ndarray, frame_rate: float = 30) -> np.ndarray:
-    """
-
-    :param spike_probability_matrix: matrix of n neuron x m samples where each element is the probability of a spike
-    :type spike_probability_matrix: np.ndarray
-    :param frame_rate: frame rate of dataset
-    :type frame_rate: float = 30
-    :return: matrix of firing rates
-    :rtype: np.ndarray
-    """
-    return spike_probability_matrix * frame_rate
-# TODO UNIT TEST
-
-
-def calculate_mean_firing_rate(firing_matrix: np.ndarray) -> np.ndarray:
-    """
-    Calculate mean firing rate
-
-    :param firing_matrix: matrix of n neuron x m samples where each element is either a spike or an
-    instantaneous firing rate
-    :type firing_matrix: np.ndarray
-    :return: 1-D vector of mean firing rates
-    :rtype: np.ndarray
-    """
-    return np.nanmean(NeuralData, axis=1)
-# TODO UNIT TEST
+def calculate_dfof(traces: np.ndarray) -> np.ndarray:
+    return
+# TODO PASTE ME, DOCUMENT, UNIT TEST
 
 
 @jit
@@ -53,4 +29,61 @@ def calculate_standardized_noise(fold_fluorescence_over_baseline: np.ndarray, fr
 # TODO UNIT TEST
 
 
-# TODO ADD PERONA MALIK DIFFUSION, NORMALIZED GAUSSIAN SMOOTHING, POLYNOMIAL DETRENDING, BASELINE CALCULATION
+@jit
+def detrend_polynomial(traces: np.ndarray) -> np.ndarray:
+    """
+    Detrend traces using a fourth-order polynomial
+
+    :param traces: matrix of traces in the form of neurons x frames
+    :type traces: np.ndarray
+    :return: detrended traces
+    :rtype: np.ndarray
+    """
+    [_neurons, _samples] = traces.shape
+    _samples_vector = np.arange(_samples)
+
+    for _neuron in range(_neurons):
+        _fit = np.polyval(np.polyfit(_samples_vector, traces[_neuron, :], deg=4), _samples_vector)
+        traces[_neuron] -= fit  # TODO AM I IN-PLACE? NEVER MATTERED BUT COULD. TEST ME
+
+    return traces
+# TODO UNIT TEST
+
+
+@jit
+def _perona_malik_diffusion(trace: np.ndarray, iters: int = 5, kappa: int = 100, gamma: float = 0.15) -> np.ndarray:
+    """
+    Edge-preserving smoothing using perona malik diffusion
+
+    :param traces: a matrix of neurons x frames
+    :type traces: np.ndarray
+    :param iters: number of iterations
+    :type iters: int = 5
+    :param kappa: diffusivity conductance
+    :type kappa: int = 100
+    :param gamma: step size (must be less than 1)
+    :type gamma: float = 0.15
+    :return: smoothed traces
+    :rtype: np.ndarray
+    """
+    return
+# TODO PASTE ME, DOCUMENT, UNIT TEST
+
+
+def smooth_perona_malik(traces: np.ndarray, iters: int = 5, kappa: int = 100, gamma: float = 0.15) -> np.ndarray:
+    """
+    Edge-preserving smoothing using perona malik diffusion
+
+    :param traces: a matrix of neurons x frames
+    :type traces: np.ndarray
+    :param iters: number of iterations
+    :type iters: int = 5
+    :param kappa: diffusivity conductance
+    :type kappa: int = 100
+    :param gamma: step size (must be less than 1)
+    :type gamma: float = 0.15
+    :return: smoothed traces
+    :rtype: np.ndarray
+    """
+    return
+# TODO PASTE ME, DOCUMENT, UNIT TEST
