4efe0a86f7f29d77b602c4cf88adecb4cf2191a3
darikoneil <d.oneil@columbia.edu>
2023-02-13 11:02:33 -0500

    automatic versioning in sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,56 +2,11 @@ import os
 import sys
 import pathlib
 
+# IMPORTS ps I can be done not so dumbly
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../'))
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'CalSciPy'
-# noinspection PyShadowingBuiltins
-copyright = "2023, Darik A. O'Neil"
-author = "Darik A. O'Neil"
-release = '0.1.4'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints']
-
-typehints_defaults = 'comma'
-templates_path = ['_templates']
-exclude_patterns = []
-
-language = '[en]'
-
-intersphinx_mapping = {
-    'cupy': ('https://docs.cupy.dev/en/stable/', None),
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://numpy.org/doc/1.24/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
-    'scikit-image': ('https://scikit-image.org/docs/stable/', None)
-
-}
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
-
-# IMPORTS
 
 module_names = (
     "bruker",
@@ -70,3 +25,37 @@ sys.path.append("".join([parent, "\\src\\CalSciPy"]))
 sys.path.append("".join([parent, "\\src"]))
 for _module in module_names:
     sys.path.append("".join([parent, "\\src\\CalSciPy\\", _module]))
+
+project = 'CalSciPy'
+# noinspection PyShadowingBuiltins
+copyright = "2023, Darik A. O'Neil"
+author = "Darik A. O'Neil"
+release = CalSciPy.version
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints']
+
+typehints_defaults = 'comma'
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = '[en]'
+
+intersphinx_mapping = {
+    'cupy': ('https://docs.cupy.dev/en/stable/', None),
+    'pandas': ('https://pandas.pydata.org/docs/', None),
+    'python': ('https://docs.python.org/3', None),
+    'numba': ('https://numba.pydata.org/', None),
+    'numpy': ('https://numpy.org/doc/1.24/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'scikit-image': ('https://scikit-image.org/docs/stable/', None)
+
+}
+
+html_theme = 'sphinx_rtd_theme'
