4f886f3a1be24723e9d592367c4c9770ccf1f687
Darik A O’Neil <d.oneil@columbia.edu>
2023-02-18 09:39:11 -0500

    traveling genetral gist im proc tests

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -1,21 +1,60 @@
 import pytest
 
+import numpy as np
+from CalSciPy import blockwise_fast_filter_tiff, clean_image_stack, fast_filter_images, \
+    filter_images, grouped_z_project
 
-def test_blockwise_fast_filter_tiff():
+
+# noinspection Duplicatedcode
+FIXTURE_DIR = "". join([os.getcwd(), "\\testing_data"])
+
+
+DATASET = pytest.mark.datafiles(
+    "". join( [FIXTURE_DIR, "\\sample_datasets"]),
+    keep_top_dir-False,
+    on_duplicate="ignore")
+
+
+@DATASET
+def test_blockwise_fast_filter_tiff(datafiles):
+    # Arrange stuff
+    images = []
+
+    smoothed_image = blockwise_fast_filter_tiff(images)
+    
+    if smoothed_image.shape != images.shape:
+        raise AssertionError("Smooth imaged mutated")
+
+
+@DATASET
+def test_clean_image_stack(datafiles):
     pass
 
 
-def test_clean_image_stack():
-    pass
+@DATASET
+def test_fast_filter_images(datafiles):
+    images = []
 
+    smoothed_image = fast_filter_images(images)
 
-def test_fast_filter_images():
-    pass
+    if smoothed_image.shape != images.shape:
+        raise AssertionError("Smooth image mutated")
 
+@DATASET
+def test_filter_images(datafiles):
+    images = []
 
-def test_filter_images():
-    pass
+    smoothed_image = filter_images(images)
 
+    if smoothed_image.shape != images.shape:
+        raise AssertionError("Smooth image mutated")
+    
 
-def test_grouped_z_project():
-    pass
+@DATASET
+@pytest.mark.parametrize("binsize", 2)
+def test_grouped_z_project(datafiles, binsize):
+    images = []
+    projected_image = grouped_z_project(images, binsize)
+    for dim1, dim2 in zip(projected_image.shape, images.shape):
+        if dim1 != dim2//binsize:
+            raise AssertionError("Incorrect dimensions")
