f4ac26fb0ef7ebcf9394b714395b8f06497aaffa
Darik A O’Neil <d.oneil@columbia.edu>
2023-02-19 22:39:31 -0500

    Update test_a_install.py

--- tests/test_a_install.py
+++ tests/test_a_install.py
@@ -6,20 +6,6 @@ import os
 from pathlib import Path
 import toml
 
-# get project information and work from correct directory
-project_dir, project_file, package_name, package_version, package_dependencies = collect_project()
-os.chdir(project_dir)
-
-
-print(f"\n{TerminalStyle.YELLOW}Package: {TerminalStyle.BLUE}{package_name}{TerminalStyle.RESET}")
-print(f"{TerminalStyle.YELLOW}Version: {TerminalStyle.BLUE}{package_version}{TerminalStyle.RESET}")
-print(f"{TerminalStyle.YELLOW}Dependencies: {TerminalStyle.BLUE}{package_dependencies}{TerminalStyle.RESET}")
-
-
-@pytest.mark.parametrize("path", [project_file])
-def test_install(path):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-e ."])
-
 
 def retrieve_details(path):
     details = toml.load(path).get("project")
@@ -47,3 +33,18 @@ def collect_project():
     project_directory = retrieve_project_directory(project_file)
     package_name, package_version, package_dependencies = retrieve_details(project_file)
     return project_directory, project_file, package_name, package_version, package_dependencies
+
+
+# get project information and work from correct directory
+project_dir, project_file, package_name, package_version, package_dependencies = collect_project()
+os.chdir(project_dir)
+
+
+print(f"\n{TerminalStyle.YELLOW}Package: {TerminalStyle.BLUE}{package_name}{TerminalStyle.RESET}")
+print(f"{TerminalStyle.YELLOW}Version: {TerminalStyle.BLUE}{package_version}{TerminalStyle.RESET}")
+print(f"{TerminalStyle.YELLOW}Dependencies: {TerminalStyle.BLUE}{package_dependencies}{TerminalStyle.RESET}")
+
+
+@pytest.mark.parametrize("path", [project_file])
+def test_install(path):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-e ."])
