86f31d08a8f82858503ab00633ae6fd9d03da45b
Darik ONeil <d.oneil@columbia.edu>
2023-08-15 09:06:53 -0400

    linting

--- tests/test_io_tools.py
+++ tests/test_io_tools.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Callable
+
 import pytest
 from tests.conftest import retrieve_dataset_ids
 from tests.helpers import read_descriptions, generate_dummy_file_name, generate_dummy_output_folder
@@ -27,38 +30,26 @@ class IO:
             np.fromfile(self.directory.joinpath("binary").joinpath("binary_video").with_suffix(".bin"), dtype=np.uint16)
         self.data = np.reshape(self.data, self.descriptions[2:])
 
-    def check_data(self, image):
+    def check_data(self, image: np.ndarray):
         np.testing.assert_array_equal(self.data, image)
 
-    def test_load_binary(self):
-        test_data = load_binary(self.directory.joinpath("binary"))
-        self.check_data(test_data)
+    def load_validator(self, function: Callable):
 
-    def test_save_binary(self):
-        save_binary(self.outputs, self.data)
+        # must fail general validation tests
+        with pytest.raises(FileNotFoundError):
+            function(Path.cwd().joinpath("arthur_dent"))
+        with pytest.raises(ValueError):
+            function("C:\\ford_prefect ***%")  # FAIL PERMITTED CHARS
+        with pytest.raises(TypeError):
+            # noinspection PyTypeChecker
+            function(125.6)  # fail with bad type so we don't do anything unexpected
 
-    def test_mutation_binary(self):
-        save_binary(self.outputs, self.data)
-        test_data = load_binary(self.outputs)
-        self.check_data(test_data)
-
-    def test_load_single_stack(self):
-        ...
-
-    def test_save_single_stack(self):
-        ...
-
-    def test_mutation_single_stack(self):
-        ...
-
-    def test_load_multi_stack(self):
-        ...
-
-    def test_save_multi_stack(self):
-        ...
-
-    def test_mutation_multi_stack(self):
-        ...
+    def save_validator(self, function: Callable):
+        with pytest.raises(ValueError):
+            function("C:\\vogon_destructor_fleet &^6*", matrix)  # FAIL VALIDATE PATH PERMITTED CHARS
+        with pytest.raises(TypeError):
+            # noinspection PyTypeChecker
+            function(125.6, matrix)  # fail with bad type so we don't do anything unexpected
 
 
 @pytest.fixture()
@@ -77,10 +68,52 @@ class TestIO:
 
     """
     def test_load_binary(self, io_helper):
-        io_helper.test_load_binary()
+        # standard load
+        test_data = load_binary(io_helper.directory.joinpath("binary"))
+        io_helper.check_data(test_data)
+        # memory map load
+        test_data = load_binary(io_helper.directory.joinpath("binary"), mapped=True)
+        io_helper.check_data(test_data)
+        # interpolate metadata load
+        test_data = load_binary(io_helper.directory.joinpath("binary"),
+                                missing_metadata={"dtype": np.uint16,
+                                                  "y": io_helper.descriptions[3],
+                                                  "x": io_helper.descriptions[4]},
+                                )
+        io_helper.check_data(test_data)
 
     def test_save_binary(self, io_helper):
-        io_helper.test_save_binary()
+        # normal save
+        save_binary(io_helper.outputs, io_helper.data)
+        # exact save
+        save_binary(io_helper.outputs, io_helper.data, "exact_filename")
 
     def test_mutation_binary(self, io_helper):
-        io_helper.test_mutation_binary()
+        # standard load-save with check for mutation
+        save_binary(io_helper.outputs, io_helper.data)
+        test_data = load_binary(io_helper.outputs)
+        io_helper.check_data(test_data)
+
+    def test_binary_exceptions(self, io_helper):
+        # fail if no dtype provided
+        with pytest.raises(AttributeError):
+            load_binary(io_helper.directory.joinpath("binary"),
+                        missing_metadata={"frames": io_helper.descriptions[2],
+                                          "y": io_helper.descriptions[3],
+                                          "x": io_helper.descriptions[4]
+                                          }
+                        )
+        # fail if only 1 of 3 shape provided
+        with pytest.raises(AttributeError):
+            load_binary(io_helper.directory.joinpath("binary"),
+                        missing_metadata={"dtype": np.uint16,
+                                          "frames": io_helper.descriptions[2]
+                                          }
+                        )
+
+        # check for general exceptions that are expected
+        io_helper.load_validator(load_binary)
+        io_helper.save_validator(save_binary)
+
+    def test_load_single_stack(self, io_helper):
+        ...
