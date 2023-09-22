029c8dda4f3891ab3c8fe953eefca3ea700673fc
darikoneil <d.oneil@columbia.edu>
2023-09-22 14:42:22 -0400

    docs

--- CalSciPy/traces/smoothing.py
+++ CalSciPy/traces/smoothing.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-
 import numpy as np
 from numba import njit
 from tqdm import tqdm
+from scipy.ndimage import gaussian_filter1d
 
 
 def perona_malik_diffusion(traces: np.ndarray,
--- CalSciPy/traces/traces.py
+++ CalSciPy/traces/traces.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Optional
 from numbers import Number
 
 import numpy as np
