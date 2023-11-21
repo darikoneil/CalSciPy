5ffef37edbd19bf9017a05514405b3631767a7b5
Darik A O’Neil <d.oneil@columbia.edu>
2023-11-21 13:13:03 -0500

    Add files via upload

--- CalSciPy/organization/__init__.py
+++ CalSciPy/organization/__init__.py
@@ -1,9 +1,16 @@
+from .subject import Subject, Mouse, Rat, Human, Fly, Fish
 from .files import FileTree, FileSet, FileMap
-
+from .imaging import ImagingExperiment
 
 __all__ = [
-    "experiment",
-    "imaging",
-    "files",
-    "subject",
+    "FileTree",
+    "FileSet",
+    "FileMap"
+    "Fish",
+    "Fly",
+    "Human",
+    "ImagingExperiment",
+    "Mouse",
+    "Rat",
+    "Subject",
 ]
